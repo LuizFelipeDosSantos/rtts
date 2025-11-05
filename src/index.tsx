cd44bbc48d2c1ed9fb22cefc1cdaeb32bf1fbc9e
Luiz Felipe dos Santos <luizfelipesantos.sc@gmail.com>
2025-11-05 14:12:45 +0100

    created fetchTodos action

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom/client";
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { App } from './components/App';
+import { reducers } from './reducers';
 
-class App extends React.Component {
-  render() {
-    return <div>Hi there</div>;
-  }
-}
+const store = createStore(reducers, applyMiddleware(thunk));
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+const el = document.getElementById("root");
+
+const root = ReactDOM.createRoot(el!);
+
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
---
+++ src/reducers/todos.ts
@@ -0,0 +1,13 @@
+import { Todo, ActionTypes } from "../actions";
+import { Action } from "../actions";
+
+export const todosReducer = (state: Todo[] = [], action: Action) => {
+  switch (action.type) {
+    case ActionTypes.fetchTodos:
+      return action.payload;
+    case ActionTypes.deleteTodo:
+      return action.payload;
+    default:
+      return state;
+  }
+};
